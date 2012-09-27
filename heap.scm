187f522cc0e02a3e2eee5704c48cb0770b5ca6b6
Peter Danenberg <pcd@roxygen.org>
2012-09-27 02:41:33 -0700

    Docs

--- heap.scm
+++ heap.scm
@@ -1,4 +1,12 @@
+@(title "Heap")
+@(description "Mutable heap with O(1) membership-testing")
+@(author "Peter Danenberg")
+@(email "pcd@roxygen.org")
+@(username "klutometis")
+
 (module heap
+  @("The heap module provides basic priority queue functions with O(1)
+membership-testing, which is useful for implementing e.g. A*.")
   (build-heap!
    make-heap
    heap-adjust-key!
