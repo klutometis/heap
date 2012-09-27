1854a1a1635aa59ae17fbda3aaab691b5701d659
Peter Danenberg <pcd@roxygen.org>
2012-09-27 05:29:02 -0700

    Test membership-testing.

--- tests/run.scm
+++ tests/run.scm
@@ -1,8 +1,8 @@
-(use heap test)
+(use debug heap test)
 
 (define (figure-6.2)
   (let ((data (list->vector (map list '(16 4 10 14 7 9 3 2 8 1)))))
-    (make-max-heap car set-car! data)))
+    (make-max-heap car set-car! cdr data)))
 
 (define (test-figure-6.2 testandum heap)
   (test testandum
@@ -10,7 +10,7 @@
         (heap-data heap)))
 
 (let ((heap (figure-6.2)))
-  (heapify! heap 1)
+  (heapify!/index heap 1)
   (test-figure-6.2 "heapify!" heap))
 
 (let ((heap (figure-6.2)))
@@ -29,10 +29,10 @@
 
 (define (figure-6.5)
   (let ((data (list->vector (map list '(16 14 10 8 7 9 3 2 4 1)))))
-    (make-max-heap car set-car! data)))
+    (make-max-heap car set-car! cdr data)))
 
 (let ((heap (figure-6.5)))
-  (heap-change-key! heap 8 15)
+  (heap-change-key!/index heap 8 15)
   (test "heap-change-key!"
         '#((16) (15) (10) (14) (7) (9) (3) (2) (8) (1))
         (heap-data heap)))
@@ -63,7 +63,17 @@
         (heap-data heap)))
 
 (let ((heap (figure-6.5)))
-  (heap-delete! heap 4)
+  (heap-delete!/index heap 4)
   (test "heap-delete!"
         '#((16) (14) (10) (8) (1) (9) (3) (2) (4) (1))
         (heap-data heap)))
+
+(let ((heap (make-max-heap)))
+  (heap-insert! heap (cons 1 'a))
+  (heap-insert! heap (cons 2 'b))
+  (heap-insert! heap (cons 3 'c))
+  (test-assert "heap-member?" (heap-member? heap 'a))
+  (heap-delete!/index heap 0)
+  (test-assert "not-heap-member? -- index" (not (heap-member? heap 'c)))
+  (heap-delete! heap 'b)
+  (test-assert "not-heap-member? -- datum" (not (heap-member? heap 'b))))
