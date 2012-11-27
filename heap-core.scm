8e5c39cb00bec54afef501a389c76062f6599af2
Peter Danenberg <pcd@roxygen.org>
2012-11-26 20:08:14 -0800

    Use > instead of =.

--- heap-core.scm
+++ heap-core.scm
@@ -129,7 +129,7 @@ overflow.")
     (make-max-heap (initial-heap-size)))
    ((initial-heap-size)
     (make-heap >
-               =
+               >
                -inf.0
                (make-vector initial-heap-size)
                0
@@ -144,7 +144,7 @@ overflow.")
     (make-min-heap (initial-heap-size)))
    ((initial-heap-size)
     (make-heap <
-               =
+               >
                +inf.0
                (make-vector initial-heap-size)
                0
